4dd390a007acf64d42aa27d4199d05742f2b1124
Erin <erin.eliz.costello@gmail.com>
2021-08-16 17:29:05 -0600

    added environment variable

--- src/api/apiNASA.js
+++ src/api/apiNASA.js
@@ -1,4 +1,4 @@
-const KEY = process.env.REACT_APP_NASA_KEY;
+const KEY = process.env.NASA_KEY;
 
 export const getNASAPhotos = () => {
   return fetch(`https://api.nasa.gov/planetary/apod?api_key=${KEY}&count=1`)
